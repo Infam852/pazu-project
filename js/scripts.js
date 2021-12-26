6c7468ef8ca5e30bf8a4a5126227a9f54cda39bc
Infam852 <djakobczak.backup@gmail.com>
2021-12-26 17:18:00 +0100

    add login/logout flow

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,34 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function checkPasswordMatch(){
+    var pswd = document.getElementById('RegisterInputPassword')
+    var pswdCheck = document.getElementById('RegisterInputPasswordConfirm')
+    pswdCheck.setCustomValidity(pswdCheck.value != pswd.value ? "Hasła muszą się zgadzać" : '')
+}
+
+
+function logout(){
+    console.log("[DEBUG] logout(): remove cookie login")
+    Cookies.remove("login")
+}
+
+
+function login(){
+    console.log("[DEBUG] login(): set cookie login=true")
+    Cookies.set('login', 'true');
+}
+
+
+function setNavbar(){
+    if (Cookies.get('login') === "true"){
+        $("#login-opt").hide();
+        $("#register-opt").hide();
+        $("#logout-opt").show();
+    } else {
+        $("#login-opt").show();
+        $("#register-opt").show();
+        $("#logout-opt").hide();
+    }
+}
